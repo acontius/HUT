a25ff1bfd351bdd3b46ec90bed830dd4ed85eb03
acontius <ahuram188@gmail.com>
2024-07-23 12:38:41 +0330

    fixed the problem

--- HUT_Processor/decoder4to16.vhd
+++ HUT_Processor/decoder4to16.vhd
@@ -1,19 +1,25 @@
 LIBRARY IEEE;
-USE IEEE.std_logic_1164.all;
-USE IEEE.numeric_std.all;
-USE WORK.utilities.all;
+USE IEEE.STD_LOGIC_1164.ALL;
+USE IEEE.STD_LOGIC_ARITH.ALL;
+USE IEEE.STD_LOGIC_UNSIGNED.ALL;
 
-entity decoder4to16 is
-    port (
-        en    : in STD_LOGIC;
-        input : in STD_LOGIC_VECTOR(3 DOWNTO 0);
-        outPut: out STD_LOGIC_VECTOR(16 DOWNTO 0)
+ENTITY decoder4to16 IS
+    PORT (
+        inputDecoder : IN STD_LOGIC_VECTOR(3 DOWNTO 0);
+        enable       : IN STD_LOGIC;
+        outDecoder   : OUT STD_LOGIC_VECTOR(15 DOWNTO 0)
     );
-end entity decoder4to16;
+END ENTITY decoder4to16;
 
-ARCHITECTURE behavioral of decoder4to16 is 
-    constant zero : STD_LOGIC_VECTOR(16 DOWNTO 0) := (OTHERS => '0');
-    constant one  : UNSIGNED(16 DOWNTO 0) := (0 => '1', OTHERS => '0');
-    BEGIN
-        outPut <= zero WHEN (en = '0') ELSE STD_LOGIC_VECTOR (SHIFT_LEFT(one,to_integer(input)));
-END ARCHITECTURE;
+ARCHITECTURE behavioral OF decoder4to16 IS
+BEGIN
+    process(inputDecoder, enable)
+    begin
+        if enable = '1' then
+            outDecoder <= (others => '0');
+            outDecoder(to_integer(unsigned(inputDecoder))) <= '1';
+        else
+            outDecoder <= (others => '0');
+        end if;
+    end process;
+END ARCHITECTURE behavioral;
