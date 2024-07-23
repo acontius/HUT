8001f1875c86372bf480957e871ec4cac8e2490a
acontius <ahuram188@gmail.com>
2024-07-23 12:37:30 +0330

    fixing the problem

--- HUT_Processor/mux16in1.vhd
+++ HUT_Processor/mux16in1.vhd
@@ -1,16 +1,18 @@
 LIBRARY IEEE;
-USE IEEE.std_logic_1164.all;
+USE IEEE.STD_LOGIC_1164.ALL;
+USE IEEE.STD_LOGIC_ARITH.ALL;
+USE IEEE.STD_LOGIC_UNSIGNED.ALL;
 
-ENTITY mux16in1 IS 
-    GENERIC (size : INTEGER);
-    PORT(
-        input : IN mux_array; 
+ENTITY mux16in1 IS
+    GENERIC (size : INTEGER := 16);
+    PORT (
+        input : IN STD_LOGIC_VECTOR(16*size-1 DOWNTO 0); 
         sel   : IN STD_LOGIC_VECTOR(3 DOWNTO 0);
-        outPut: OUT STD_LOGIC_VECTOR(16 DOWNTO 0)
+        outPut: OUT STD_LOGIC_VECTOR(size-1 DOWNTO 0)
     );
-END ENTITY; --Mux 16/1
+END ENTITY mux16in1;
 
-ARCHITECTURE behavioral OF mux16in1 IS 
-    BEGIN
-        outPut <= input(bin2int(sel));
-END ARCHITECTURE;
+ARCHITECTURE behavioral OF mux16in1 IS
+BEGIN
+    outPut <= input((to_integer(unsigned(sel)) + 1) * size - 1 DOWNTO to_integer(unsigned(sel)) * size);
+END ARCHITECTURE behavioral;
