611a57941d591601cd78a0db792469c7d58dae3e
acontius <ahuram188@gmail.com>
2024-08-05 00:03:56 +0330

    fixed the bug tested=true

--- HUT_Processor/data_mem.vhd
+++ HUT_Processor/data_mem.vhd
@@ -3,13 +3,13 @@ USE IEEE.NUMERIC_STD.ALL;
 USE IEEE.STD_LOGIC_1164.ALL;
 
 ENTITY ram IS 
-    GENERIC (adr_size, data_size : INTEGER);
+    GENERIC (adr_size, data_size : INTEGER := 16);
     PORT(
         clk, we : IN STD_LOGIC;
         adr     : IN STD_LOGIC_VECTOR(adr_size - 1 DOWNTO 0);
         d_in    : IN STD_LOGIC_VECTOR(data_size - 1 DOWNTO 0);
         d_out   : OUT STD_LOGIC_VECTOR(data_size - 1 DOWNTO 0)
-    );
+    );  
 END ENTITY ram;
 
 ARCHITECTURE behavioral OF ram IS 
